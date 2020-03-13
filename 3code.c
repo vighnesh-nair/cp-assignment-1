4c8f3ef13327528d4432b9c72f873a5a13e208dd
vighnesh-nair <62141079+vighnesh-nair@users.noreply.github.com>
2020-03-13 16:37:52 +0400

    Create 3code.c

---
+++ 3code.c
@@ -0,0 +1,41 @@
+/* to stimulate a simple calculator*/
+
+#include<stdio.h>
+
+void main()
+	{
+		float result,num1.num2;
+		char operator;
+
+		printf("\n Enter an Operator (+,-,*,/) : ");
+		scanf("%c", &operator);
+		prinrf("\n Enter 2 operands: ");
+		scanf("%f%f", &num1, &num2);
+
+		switch(operator)
+			{
+				case '+':
+					result=num1+num2;
+					printf(" Sum = %f", result);
+					break;
+
+				case '-':
+					result=num1-num2;
+					printf(" Difference = %f", result);
+					break;
+
+				case '*':
+					result=num1*num2;
+					printf(" Product = %f", result);
+					break;
+
+				case '/':
+					result=num1/num2;
+					printf(" Quotient = %f", result);
+					break;
+
+				default:
+					printf("\n ERROR! Invalid Operator!");
+			}
+
+	}
