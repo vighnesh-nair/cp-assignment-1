b21631546dee4f8b80f6b42103378a972218982e
vighnesh-nair <62141079+vighnesh-nair@users.noreply.github.com>
2020-03-13 16:46:35 +0400

    Create 14code.c

---
+++ 14code.c
@@ -0,0 +1,19 @@
+/* to print the factorial of a given number */
+
+#include<stdio.h>
+
+void main()
+	{
+		int num;
+
+		printf("\n Enter number whose factorial is to be found: ");
+		scanf("%d", &num);
+	
+		for(int i=1;i<num;i++)
+			{
+				num*=i;
+			}
+
+		printf("\n Factorial = %d", num);
+
+	}
