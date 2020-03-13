4a8364d98fcd4ce1c2a4179d55f5226f5a9a9e22
vighnesh-nair <62141079+vighnesh-nair@users.noreply.github.com>
2020-03-13 16:38:28 +0400

    Create 4code.c

---
+++ 4code.c
@@ -0,0 +1,21 @@
+/* to generate the Fibonacci Series*/
+
+#include<stdio.h>
+
+void main()
+	{
+		int n,t1=0,t2=1,t3;
+
+		printf("\n Enter the number of terms: ");
+		scanf("%d",&n);
+		printf("\n Fibonacci Series: ");
+
+		for(int i=1;i<=n;i++)
+			{
+				printf("%d",t1);
+				t3=t1+t2;
+				t1=t2;
+				t2=t3;
+			}
+
+	}
